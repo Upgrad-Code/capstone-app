2cb526476d4d275f56eee833e67478d5cdd978e4
Upgrad-Code <112937791+Upgrad-Code@users.noreply.github.com>
2023-06-08 12:51:11 +0530

    1 file modified, 2 added

---
+++ src/utils/config.utils.js
@@ -0,0 +1,2 @@
+export const TIMEOUT_SEC = 10;
+export const COUNTRIES_API_BASE_URL = 'https://restcountries.com/v3.1';
---
+++ src/utils/helpers.utils.js
@@ -0,0 +1,40 @@
+import { TIMEOUT_SEC } from './config.utils';
+
+const timeout = (sec) => {
+  return new Promise((_, reject) => {
+    return setTimeout(() => {
+      return reject(`Request Timeout... More than ${sec}`);
+    }, sec * 1000);
+  });
+};
+
+export const apiRequest = async (
+  url,
+  signal,
+  uploadData = undefined,
+  errMsg = 'Something went wrong...'
+) => {
+  try {
+    const fetchPro = uploadData
+      ? fetch(url, {
+          method: 'POST',
+          signal: signal,
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+          body: JSON.stringify(uploadData),
+        })
+      : fetch(url, {
+          method: 'GET',
+          signal: signal,
+        });
+    const resposne = await Promise.race([timeout(TIMEOUT_SEC), fetchPro]);
+    const data = await resposne.json();
+    if (!resposne.ok) {
+      throw new Error(`${errMsg} ${resposne.status} ${data.message}`);
+    }
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
