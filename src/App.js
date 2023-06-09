aaacbc5ebcbeebc7ef315b7663e987dbac1fe69a
Upgrad-Code <112937791+Upgrad-Code@users.noreply.github.com>
2023-06-09 17:08:15 +0530

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Countries from './routes/countries/countries.component';
+import NotFound from './routes/not-found/not-found.component';
 import './style.css';
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/countries" element={<Countries />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
---
+++ src/routes/not-found/not-found.component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const NotFound = () => {
+  return (
+    <Container>
+      <Row>
+        <Col md={12}>
+          <p>
+            Page Not Found.....................................................
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default NotFound;
