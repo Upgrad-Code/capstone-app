35d49134b8532d760604740e825fe63283c8f7eb
Upgrad-Code <112937791+Upgrad-Code@users.noreply.github.com>
2023-06-16 18:59:17 +0530

    3 files modified

--- src/components/countries-card/countries-card.component.js
+++ src/components/countries-card/countries-card.component.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Card, Button } from 'react-bootstrap';
+import './countries-card.styles.scss';
 
 export const CountriesCard = ({ country }) => {
   return (
-    <div className="col-md-3">
-      <Card>
-        <Card.Body>
-          <Card.Text>{country.name.common}</Card.Text>
-          <Link to={country.cca3}>
-            <Button variant="primary">Go</Button>
-          </Link>
-        </Card.Body>
-      </Card>
+    <div className="col-md-3 mb-3">
+      <div className="countries-card">
+        <Card className="shadow-sm">
+          <Card.Body>
+            <Card.Text>{country.name.common}</Card.Text>
+            <Link to={country.cca3}>
+              <Button variant="primary">Go</Button>
+            </Link>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
   );
 };
